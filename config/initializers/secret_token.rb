03c6e04b80fe01ad7b1fac05b76f493388c1913d
Deidre Bloomer-Daly <deebloomer@hotmail.com>
2012-11-15 16:53:29 +0000

    laptop shop tutorials 1 and 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaptopShop::Application.config.secret_token = '8f7a5059fc5082d8bc2e24e391dbaa579cfc951562e60d0183ccfa734295e711688f3c4ec58703697d61eecc5ae86b4cbebc8856a7fb1c42b259ab620923376a'
